048e310fab14e00bcee1e7781b38b43621c93170
preetham-p-m <preethampm1@gmail.com>
2024-09-09 16:32:49 +0530

    implemented generic exception handler

--- restful-web-service/src/main/java/com/pmp/restful_web_service/exception/CustomExceptionHandler.java
+++ restful-web-service/src/main/java/com/pmp/restful_web_service/exception/CustomExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,13 +17,19 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Error> handleAllException(Exception ex, WebRequest request) throws Exception {
-        return new ResponseEntity<Error>(getError(ex, request), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<Error>(getError(ex, request), getErrorCode(ex));
     }
 
+    /**
+     * The below method will be redundant since the handleAllException method
+     * provides a generic implementation to return the appropriate status code
+     * using reflection.
+     */
     @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Error> handleUserNotFoundExceptionException(Exception ex, WebRequest request)
             throws Exception {
-        return new ResponseEntity<Error>(getError(ex, request), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<Error>(getError(ex, request),
+                HttpStatus.NOT_FOUND);
     }
 
     private static Error getError(Exception ex, WebRequest request) {
@@ -30,6 +37,11 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
                 LocalDateTime.now(),
                 ex.getMessage(),
                 request.getDescription(false));
+    }
+
+    private static HttpStatus getErrorCode(Exception ex) {
+        ResponseStatus responseStatus = ex.getClass().getAnnotation(ResponseStatus.class);
+        return responseStatus == null ? HttpStatus.INTERNAL_SERVER_ERROR : responseStatus.code();
     }
 
 }
