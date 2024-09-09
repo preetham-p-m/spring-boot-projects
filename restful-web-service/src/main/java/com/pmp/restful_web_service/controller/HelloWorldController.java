c0ba7a7f58ada3378af03695faa3d4db6e3f05c9
preetham-p-m <preethampm1@gmail.com>
2024-09-09 20:45:26 +0530

    enabled swagger, xml response, i18n

--- restful-web-service/src/main/java/com/pmp/restful_web_service/controller/HelloWorldController.java
+++ restful-web-service/src/main/java/com/pmp/restful_web_service/controller/HelloWorldController.java
@@ -4,15 +4,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pmp.restful_web_service.model.HelloWorld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloWorldController {
 
+    MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        super();
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("/hello-i18n")
+    public String helloUserI18n() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return this.messageSource.getMessage("hello.message", null,
+                "The message will be displayed if the key is not present in the default language", locale);
     }
 
     @GetMapping("/hello-world")
