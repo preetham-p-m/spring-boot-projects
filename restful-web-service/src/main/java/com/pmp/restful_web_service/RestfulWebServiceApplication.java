c5241ec364ec24e608625ce70f15a1af44ee624f
preetham-p-m <preethampm1@gmail.com>
2024-09-30 20:20:04 +0530

    cors

--- restful-web-service/src/main/java/com/pmp/restful_web_service/RestfulWebServiceApplication.java
+++ restful-web-service/src/main/java/com/pmp/restful_web_service/RestfulWebServiceApplication.java
@@ -2,12 +2,25 @@ package com.pmp.restful_web_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServiceApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry corsRegistry) {
+				corsRegistry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
