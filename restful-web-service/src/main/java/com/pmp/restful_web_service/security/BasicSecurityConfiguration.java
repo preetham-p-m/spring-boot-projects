49f05baeddde6d2092571381c8912197303e7dc6
preetham-p-m <preethampm1@gmail.com>
2024-10-08 18:43:05 +0530

    added basic auth and cors

--- restful-web-service/src/main/java/com/pmp/restful_web_service/security/BasicSecurityConfiguration.java
+++ restful-web-service/src/main/java/com/pmp/restful_web_service/security/BasicSecurityConfiguration.java
@@ -1,25 +1,21 @@
 package com.pmp.restful_web_service.security;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-public class SecurityConfiguration {
-
+// @Configuration
+public class BasicSecurityConfiguration {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
         // Authenticate all requests
-        httpSecurity
-                .authorizeHttpRequests(
-                        auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-                                .anyRequest()
-                                .authenticated());
+        httpSecurity.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().authenticated());
 
         // Added basic popup to show the login
         httpSecurity.httpBasic(Customizer.withDefaults());
