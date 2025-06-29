ff9a19479271c1f6e327b60f2c04acb49c889f4c
preetham-p-m <preethampm1@gmail.com>
2025-06-29 11:04:51 +0530

    Remove mono-tweet project files including pom.xml, application configuration, controllers, models, services, repositories, exception handling, and test cases. This commit effectively deletes the entire mono-tweet application codebase.

--- flagforge/src/main/java/com/pmp/flagforge/Model/FlagStatus.java
+++
@@ -1,5 +0,0 @@
-package com.pmp.flagforge.Model;
-
-public enum FlagStatus {
-    ACTIVE, INACTIVE, ARCHIVED
-}
--- mono-tweet/src/main/java/com/pmp/mono_tweet/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pmp.mono_tweet.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pmp.mono_tweet.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
